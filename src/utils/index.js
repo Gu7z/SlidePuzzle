94bbe0189a07b292abde4c08ae022fdaeb60b41b
Gu7z <gustavoferri13@gmail.com>
2022-02-14 21:56:47 -0400

    cria um index para o utils

---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+export { default as findLastValue } from "./find_last_value";
+export { default as transformArrayValuesIn2D } from "./transform_array_in_2d";
+export { default as isValidMoviment } from "./is_valid_moviment";
+export { default as swapSquares } from "./swap_square";
