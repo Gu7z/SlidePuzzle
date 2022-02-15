1de9396399d566c626d19d48b82ba28f59ca1b9c
Gu7z <gustavoferri13@gmail.com>
2022-02-14 21:57:23 -0400

    utiliza o utils para gerar, randomizar e executar os movimentos do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import React, { useCallback, useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Quadrado from "./components/quadrado";
 import Drop from "./components/drop";
+import { swapSquares } from "./utils";
 
 const App = () => {
-  const [gameSize, setGameSize] = useState(undefined);
-  const [squares, setSquares] = useState([]);
-  const [positions, setPositions] = useState([]);
+  const [gameSize, setGameSize] = useState(3);
+  const [squaresValues, setSquaresValues] = useState([]);
 
-  const generateSquares = useCallback(() => {
-    const newSquares = [];
-    const newPositions = [];
+  const generateSquares = () => {
+    let newSquaresValues = [];
 
     for (let i = 1; i <= gameSize ** 2; i++) {
-      newSquares.push(
-        <Quadrado
-          moveSquare={() => {}}
-          position={i}
-          squaresPerLine={gameSize}
-        />
-      );
-
-      newPositions.push(i);
+      newSquaresValues.push(i);
     }
 
-    setSquares(newSquares);
-    setPositions(newPositions);
-  }, [gameSize]);
+    const numberOfRandomizes = 1000 * (gameSize ** 2 / 2);
+
+    for (let index = 1; index <= numberOfRandomizes; index++) {
+      const randomNumber = Math.floor(Math.random() * gameSize ** 2 + 1);
+
+      newSquaresValues = swapSquares(newSquaresValues, gameSize, randomNumber);
+    }
+
+    setSquaresValues(newSquaresValues);
+  };
+
+  const moveSquare = (squareValue) => {
+    const newSquaresValues = swapSquares(squaresValues, gameSize, squareValue);
+
+    setSquaresValues(newSquaresValues);
+  };
 
   useEffect(() => {
     if (gameSize === undefined) return;
 
     generateSquares();
-  }, [generateSquares, gameSize]);
+  }, [gameSize]);
 
   return (
     <div style={{ alignContent: "center" }} className="App">
@@ -47,8 +52,14 @@ const App = () => {
           flexWrap: "wrap",
         }}
       >
-        {squares.map((squares, index) => (
-          <div key={`square-${index}`}>{squares}</div>
+        {squaresValues.map((squareValue, index) => (
+          <div key={`square-${index}`}>
+            <Quadrado
+              moveSquare={moveSquare}
+              squareValue={squareValue}
+              squaresPerLine={gameSize}
+            />
+          </div>
         ))}
       </div>
     </div>
