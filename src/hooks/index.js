49399a9fae7665798a68ee9a4c4661846cd33d77
Gu7z <gustavoferri13@gmail.com>
2022-02-14 22:44:37 -0400

    add is_mobile hook

---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as isMobile } from "./is_mobile";
---
+++ src/hooks/is_mobile.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const IsMobile = (props) => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return width <= 768;
+};
